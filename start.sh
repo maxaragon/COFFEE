dc10ea527c3429cce6ca7a20398d59b8187a442d
maxaragon <max_aragon7@hotmail.com>
2022-07-30 14:51:41 +0000

    udpate to env_opnc

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 conda install -c conda-forge nb_conda_kernels
 #conda create --name env-wekeo ipykernel -y 
 conda env create -f env_opnc.yml ipykernel -y
-conda activate env-wekeo 
+conda activate env_opnc
 python -m ipykernel install --user --name env-wekeo
 
 #conda init
