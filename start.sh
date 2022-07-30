22cda8e348f70e1f01cc6287e3ad036d7a5ae6bd
maxaragon <max_aragon7@hotmail.com>
2022-07-30 15:00:37 +0000

    updated to env_opnc

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #conda update -n base conda -y
 conda install -c conda-forge nb_conda_kernels
-conda env create -f env_opnc.yml ipykernel -y
+conda env create -f env_opnc.yml ipykernel
 conda activate env_opnc
 python -m ipykernel install --user --name env_opnc
