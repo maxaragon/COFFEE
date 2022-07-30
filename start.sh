e6dd93af2e79ea0d9f7c4111511cbbfc3a8131f4
maxaragon <max_aragon7@hotmail.com>
2022-07-30 14:49:46 +0000

    updated with env_opnc.yml

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #conda update -n base conda -y
 conda install -c conda-forge nb_conda_kernels
-conda create --name env-wekeo ipykernel -y 
+#conda create --name env-wekeo ipykernel -y 
+conda env create -f env_opnc.yml ipykernel -y
 conda activate env-wekeo 
 python -m ipykernel install --user --name env-wekeo
 
