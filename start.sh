9688d29172a12865fd1881e5a10faa15c3871f2d
maxaragon <max_aragon7@hotmail.com>
2022-07-30 14:17:05 +0000

    kernel and env init

---
+++ start.sh
@@ -0,0 +1,7 @@
+#conda update -n base conda -y
+conda install -c conda-forge nb_conda_kernels
+conda create --name env-wekeo ipykernel -y 
+conda activate env-wekeo 
+python -m ipykernel install --user --name env-wekeo
+
+#conda init
