610d01ea858fec15a04a2ae3d4f061b1953e42cb
Max <39450418+maxaragon@users.noreply.github.com>
2022-07-31 23:11:18 +0200

    Delete .ipynb_checkpoints directory

--- .ipynb_checkpoints/start-checkpoint.sh
+++
@@ -1,5 +0,0 @@
-conda env create -f coffee.yml ipykernel
-#source /opt/conda/etc/profile.d/conda.sh
-eval "$(conda shell.bash hook)"
-conda activate coffee.yml
-python -m ipykernel install --user --name coffee.yml
