eae30e547628586996609532c58cf6220dca480f
Max <39450418+maxaragon@users.noreply.github.com>
2022-08-01 04:00:16 +0200

    start with data

--- start.sh
+++ start.sh
@@ -3,3 +3,8 @@ conda env create -f coffee.yml ipykernel
 eval "$(conda shell.bash hook)"
 conda activate coffee
 python -m ipykernel install --user --name coffee
+mkdir data
+cd data
+wget --max-redirect=20 -O data.zip https://www.dropbox.com/sh/l3xu7bv91946hwt/AAAV_6oscivAbSpyTpRQKJq4a?dl=1
+unzip data.zip
+rm -r data.zip
